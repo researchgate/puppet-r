7ad4b9e22676a56951cbe370c25cb32a2c4737a3
Michael Renner <michael.renner@researchgate.net>
2015-08-20 18:34:18 +0200

    hackaround

--- manifests/package.pp
+++ manifests/package.pp
@@ -1,14 +1,15 @@
-define r::package($r_path = "/usr/bin/R", $repos = ["http://cran.rstudio.com"], $dependencies = false) {
+define r::package($r_path = "/usr/bin/R", $repos = "'http://cran.rstudio.com'", $dependencies = false) {
 
   $depopt = $dependencies ? {
     true => 'TRUE',
     default => 'FALSE'
   }
 
-  $repostring = join($repos.map |key| { "'$key'"}, ",")
+   # FIXME - we need to build a proper array of repositories
+   #  $repostring = join($repos.map |key| { "'$key'"}, ",")
 
   exec { "install_r_package_$name":
-    command => "$r_path -e \"install.packages('$name', repos=c($repostring), dependencies = $depopt)\"",
+    command => "$r_path -e \"install.packages('$name', repos=c($repos), dependencies = $depopt)\"",
     unless  => "$r_path -q -e '\"$name\" %in% installed.packages()' | grep 'TRUE'",
     require => Class['r']
   }
