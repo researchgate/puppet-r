9f47248057107e3dcb17d3aca26e14fd8e7e0135
Michael Renner <michael.renner@researchgate.net>
2015-08-20 18:26:53 +0200

    trying multiple repo support

--- manifests/package.pp
+++ manifests/package.pp
@@ -1,10 +1,14 @@
-define r::package($r_path = "/usr/bin/R", $repo = "http://cran.rstudio.com", $dependencies = false) {
+define r::package($r_path = "/usr/bin/R", $repos = ["http://cran.rstudio.com"], $dependencies = false) {
+
+  $depopt = $dependencies ? {
+    true => 'TRUE',
+    default => 'FALSE'
+  }
+
+  $repostring = join($repos.map |key| { "'$key'"}, ",")
 
   exec { "install_r_package_$name":
-    command => $dependencies ? { 
-      true    => "$r_path -e \"install.packages('$name', repos='$repo', dependencies = TRUE)\"",
-      default => "$r_path -e \"install.packages('$name', repos='$repo', dependencies = FALSE)\""
-    },
+    command => "$r_path -e \"install.packages('$name', repos=c($repostring), dependencies = $depopt)\"",
     unless  => "$r_path -q -e '\"$name\" %in% installed.packages()' | grep 'TRUE'",
     require => Class['r']
   }
